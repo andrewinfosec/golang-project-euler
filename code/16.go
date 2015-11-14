7bbfd232bea56efe5edfe7266b3bb6c0624c7c13
andrewinfosec <andrewinfosec@gmail.com>
2015-11-14 11:50:54 -0800

    go fmt

--- code/16.go
+++ code/16.go
@@ -3,24 +3,24 @@
 package main
 
 import (
-  "fmt"
-  "strconv"
-  "math/big"
+	"fmt"
+	"math/big"
+	"strconv"
 )
 
 func digitSum(s string) (sum int) {
-  sum = 0
-  for _, c := range s {
-    i, err := strconv.Atoi(string(c))
-    if err != nil {
-      panic(err)
-    }
-    sum += i
-  }
-  return sum
+	sum = 0
+	for _, c := range s {
+		i, err := strconv.Atoi(string(c))
+		if err != nil {
+			panic(err)
+		}
+		sum += i
+	}
+	return sum
 }
 
 func main() {
-    n := new(big.Int).Exp(big.NewInt(2), big.NewInt(1000), nil).String()
-    fmt.Println(digitSum(n)) // 1366
+	n := new(big.Int).Exp(big.NewInt(2), big.NewInt(1000), nil).String()
+	fmt.Println(digitSum(n)) // 1366
 }
