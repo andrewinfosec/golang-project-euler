7b87befdd687af7d625ec6f5b2d677403503ac06
andrewinfosec <andrewinfosec@gmail.com>
2015-11-14 10:02:47 -0800

    go fmt

--- code/15.go
+++ code/15.go
@@ -7,13 +7,13 @@ import "fmt"
 const gridSize int = 20
 
 func main() {
-  var paths int = 1
+	var paths int = 1
 
-  // "2*n choose n" becomes "40 choose 20"
-  // https://en.wikipedia.org/wiki/Binomial_coefficient
-  for i := 0; i < gridSize; i++ {
-    paths = paths * ((2 * gridSize) - i)
-    paths = paths / (i + 1)
-  }
-  fmt.Println(paths) // 137846528820
+	// "2*n choose n" becomes "40 choose 20"
+	// https://en.wikipedia.org/wiki/Binomial_coefficient
+	for i := 0; i < gridSize; i++ {
+		paths = paths * ((2 * gridSize) - i)
+		paths = paths / (i + 1)
+	}
+	fmt.Println(paths) // 137846528820
 }
